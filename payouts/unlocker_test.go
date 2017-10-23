ec45dd1a561fd802f7afe1ec8769e3f5330adfda
Georgi Chompalov <7721127+Cremator@users.noreply.github.com>
2017-10-23 15:07:17 +0300

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Cremator/open-ethereum-pool/rpc"
-	"github.com/Cremator/open-ethereum-pool/storage"
+	"../rpc"
+	"../storage"
 )
 
 func TestMain(m *testing.M) {
